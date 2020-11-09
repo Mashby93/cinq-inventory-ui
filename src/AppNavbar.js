250e8732a6fb290afa4c5f111125e4a3b48b6151
Mike <mike.ashby93@gmail.com>
2020-11-08 21:06:44 -0500

    Basic authentication

--- src/AppNavbar.js
+++ src/AppNavbar.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import AuthService from "./AuthService";
 
 export default class AppNavbar extends Component {
   constructor(props) {
     super(props);
-    this.state = {isOpen: false};
+    this.state = {
+      isOpen: false,
+      user:AuthService.getCurrentUser()
+    };
     this.toggle = this.toggle.bind(this);
   }
 
   toggle() {
     this.setState({
-      isOpen: !this.state.isOpen
+      isOpen: !this.state.isOpen,
+      user:this.state.user
     });
   }
 
   render() {
+    const user = this.state.user;
 
     return <Navbar color="dark" dark expand="md" sticky="top">
       <NavbarBrand tag={Link} to="/"><a href="#" class="pull-left"><img src={process.env.PUBLIC_URL + '/cinq-logo.png'} height="50" width="50"></img></a> </NavbarBrand>
