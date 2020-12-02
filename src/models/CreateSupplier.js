b2cbed233cae1517a6ccc9145d7b141749d44ae9
Mike <mike.ashby93@gmail.com>
2020-12-02 09:44:58 -0500

    Start Create Supplier form

---
+++ src/models/CreateSupplier.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+
+import UserService from "../services/UserService";
+
+const required = value => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
+    );
+  }
+};
+
+export default class CreateSupplier extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeName = this.onChangeName.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      name: "",
+      loading: false
+    };
+  }
+
+
+  onChangeName(e) {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.setState({
+      message: "",
+      loading: true
+    });
+
+    this.form.validateAll();
+
+    if (this.checkBtn.context._errors.length === 0) {
+      axios.post(API_URL, {
+         params: {
+           name: this.state.name
+         }
+       });
+    }
+  }
+
+  render() {
+    return (
+      <div className="col-md-12">
+        <div className="card card-container">
+          <Form
+            onSubmit={this.handleLogin}
+            ref={c => {
+              this.form = c;
+            }}
+          >
+
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <Input
+                type="text"
+                className="form-control"
+                name="name"
+                value={this.state.name}
+                onChange={this.onChangeName}
+                validations={[required]}
+              />
+            </div>
+
+            <div className="form-group">
+              <button
+                className="btn btn-primary btn-block"
+                disabled={this.state.loading}
+              >
+                {this.state.loading && (
+                  <span className="spinner-border spinner-border-sm"></span>
+                )}
+                <span>Save</span>
+              </button>
+            </div>
+
+            {this.state.message && (
+              <div className="form-group">
+                <div className="alert alert-danger" role="alert">
+                  {this.state.message}
+                </div>
+              </div>
+            )}
+            <CheckButton
+              style={{ display: "none" }}
+              ref={c => {
+                this.checkBtn = c;
+              }}
+            />
+          </Form>
+        </div>
+      </div>
+    );
+  }
+}
