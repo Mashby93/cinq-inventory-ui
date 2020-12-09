b621c316ec6f56f923d360cceb3d2e834f00227a
Michael Ashby <mike.ashby93@gmail.com>
2020-12-09 13:50:31 -0500

    Test partial url

--- src/AuthService.js
+++ src/AuthService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/authentication/";
+const API_URL = "/api/authentication/";
 
 class AuthService {
   login(username, password) {
