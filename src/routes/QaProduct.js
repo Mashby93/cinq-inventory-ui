bcc6b4f8f63a86455334891fe36417920613c5f5
Michael Ashby <mike@Michaels-MacBook-Air.local>
2020-12-15 11:47:43 -0500

    Add notes to QA page and fix empty checklist issue

--- src/routes/QaProduct.js
+++ src/routes/QaProduct.js
@@ -3,6 +3,8 @@ import { Link, withRouter } from 'react-router-dom';
 import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap';
 import AppNavbar from '../AppNavbar';
 
+import ProductService from '../services/ProductService';
+
 class QaProduct extends Component {
 
   emptyItem = {
@@ -13,7 +15,7 @@ class QaProduct extends Component {
     plot: {},
     supplier: {},
     metadata: {
-      notes: {},
+      notes: [],
       errorCode: {
         id: '',
         code: '',
@@ -32,10 +34,12 @@ class QaProduct extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      item: this.emptyItem
+      item: this.emptyItem,
+      note:""
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleAddNote = this.handleAddNote.bind(this);
   }
 
   async componentDidMount() {
@@ -58,6 +62,22 @@ class QaProduct extends Component {
         }
     });
     this.setState({item});
+  }
+
+  handleAddNote(event) {
+    event.preventDefault();
+    let {item} = this.state;
+    let promise = ProductService.addNote(item.id, this.state.note);
+
+    promise.then(data => {
+      item.metadata.notes = data;
+    });
+
+    this.setState({item: item, errorCodes: this.state.errorCodes, note:""});
+  }
+
+  handleChangeNote(event) {
+    this.setState({note: event.target.value})
   }
 
   async handleReject(item) {
@@ -92,6 +112,10 @@ class QaProduct extends Component {
 
   render() {
     const {item} = this.state;
+
+    const notes = item.metadata.notes.map(n => {
+      return <div> {n} </div>
+    });
 
     const checkList = item.metadata.checkList === undefined || item.metadata.checkList === null || item.metadata.checkList.items === undefined || item.metadata.checkList.items === null ? "" : item.metadata.checkList.items.map(i => {
       let data = Object.keys(i.items).map((k, t) => {
@@ -137,6 +161,14 @@ class QaProduct extends Component {
         <h4> Model Number: {item.model.modelNumber} </h4>
         <h4> Serial Number: {item.serialNumber} </h4>
         <h4> Error Code: {item.metadata.errorCode ? item.metadata.errorCode.code + " - " + item.metadata.errorCode.description : ""} </h4>
+        <h4> Notes:</h4>
+        {notes}
+        <Form onSubmit={this.handleAddNote}>
+        <FormGroup>
+          <Input type="text" name="note" id="note" onChange={(event) => this.handleChangeNote(event)}/>
+          <Button color="primary" type="submit">Add</Button>
+        </FormGroup>
+        </Form>
         </td>
         </tr>
       </Container>
